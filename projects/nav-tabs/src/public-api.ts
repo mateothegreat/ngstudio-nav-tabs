953efb879beae536ba3573674840ddfa6020a98f
Matthew Davis <matthew@matthewdavis.io>
2022-04-07 20:53:42 -0400

    bump

--- projects/nav-tabs/src/public-api.ts
+++ projects/nav-tabs/src/public-api.ts
@@ -1,7 +1,6 @@
-/*
- * Public API Surface of nav-tabs
- */
-
 export * from './lib/nav-tabs.service';
 export * from './lib/nav-tabs.component';
 export * from './lib/nav-tabs.module';
+export * from './lib/nav-tabs-config';
+export * from './lib/nav-tabs-config-item';
+export * from './lib/nav-tab-instance';
